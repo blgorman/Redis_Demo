8e8dd2b6beb56de6e6ebccc7d15226feba60b96b
Brian L. Gorman <brian@farreachinc.com>
2019-03-22 22:45:45 -0500

    consoledefault

--- Newsletter/Program.cs
+++ Newsletter/Program.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace Newsletter
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
+
+            Console.WriteLine("Completed");
+            Console.ReadLine();
         }
     }
 }
